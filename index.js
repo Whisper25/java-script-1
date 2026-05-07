c36d7e4713b0e8e624e32f94f8c1740b2d2425b6
Whisper25 <whisper252552@gmail.com>
2026-05-07 20:14:40 +0200

    const

---
+++ index.js
@@ -0,0 +1,33 @@
+console.log('name');
+let firstName = 'Olena';
+console.log(firstName);
+let birthDate = 1997;
+let favColor = 'red';
+console.log(firstName, favColor);
+
+let isAdult = true;
+let isWeekend = false;
+
+
+// undefined
+let age;
+console.log(age)
+//
+
+let pet = 'null';
+
+const titleMovie = 'Ice Age';
+console.log(titleMovie);
+
+
+const clockTime = ('8:00');
+console.log(clockTime);
+const weekDaysCount = 7;
+console.log(weekDaysCount);
+
+const isEmployed = false;
+console.log(isEmployed);
+
+const isCar = false;
+console.log
+
